31e72d592c8551ce188f4b02ed8e2fb6ce17b20a
PSAbhiram045 <psabhiram2006@gmail.com>
2026-05-30 22:52:38 +0530

    Fixed Vite config for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             "/api": {
-                target: /*"http://localhost:5000"*/import.meta.env.VITE_API_URL,
+                target: "http://localhost:5000",
                 changeOrigin: true,
             },
         },
